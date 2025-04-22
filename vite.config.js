53099fafd47fe95dddd5349908fd1623ea99f01a
zhoul8 <zhoul3419@my.sduhsd.net>
2025-04-22 09:33:41 -0700

    hi

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/whatvr-u-want/'
-})
+  base:'/whatvr-u-want/',
+});
