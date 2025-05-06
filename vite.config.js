67ac8e4d91e37e7c1337a2af7b069849da5c31c2
zhoul8 <zhoul3419@my.sduhsd.net>
2025-05-06 09:17:08 -0700

    g

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-  base: 'zhoul8/whatvr-u-want', //Replace with your GitHub repo name
+  base: 'whatvr-u-want', //Replace with your GitHub repo name
 });
